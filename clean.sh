c4e1e1267146d2c4018546ef8130dd47fdf5eb9c
Tobias Derksen <tobias.derksen@codecentric.de>
2018-05-07 11:58:34 +0200

    Fixed loading of configuration variables

--- clean.sh
+++ clean.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # fetch OPENSHIFT_UNIT_NAMESPACE and OPENSHIFT_UNIT_NAME first
-. exports
+. ./exports
 
 oc delete --ignore-not-found project/${OPENSHIFT_UNIT_NAMESPACE}
 oc delete --ignore-not-found clusterrolebinding/${OPENSHIFT_UNIT_NAME}
