43b7ba40980564b19c8876de15817bf40297e08e
Gerald Schmidt <gerald1248@gmail.com>
2018-05-07 09:04:16 +0200

    fixed name refs

--- clean.sh
+++ clean.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
+
+# fetch OPENSHIFT_UNIT_NAMESPACE and OPENSHIFT_UNIT_NAME first
+. exports
+
 oc delete --ignore-not-found project/${OPENSHIFT_UNIT_NAMESPACE}
 oc delete --ignore-not-found clusterrolebinding/${OPENSHIFT_UNIT_NAME}
